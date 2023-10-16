15c16f095675fcffc33753cf593cec462cf43078
Kranti Ponala <ponala.kranti@gmail.com>
2023-10-15 22:38:09 -0400

    first commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+
+  public counter = 0;
+  handleCounter(){
+    this.counter += 1;
+  }
+}
